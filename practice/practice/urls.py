218ab2d91b346baac9feb1b55be51134163515f0
Pawel-Truszkowski <pawel.truszkowski14@gmail.com>
2026-05-18 20:07:57 +0200

    Completed task 01

--- practice/practice/urls.py
+++ practice/practice/urls.py
@@ -21,11 +21,15 @@ from rest_framework_simplejwt.views import (
     TokenRefreshView,
     TokenVerifyView,
 )
+from api.views import protected_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
-    path('api/', include('api.urls')),
+]
+
+urlpatterns += [
+    path('api/secure/', protected_view, name='secure'),
 ]
