6bf0b94faaea21e748f75d9d7f37b5f7b88595ab
Pawel-Truszkowski <pawel.truszkowski14@gmail.com>
2026-05-20 19:39:03 +0200

    Completed task 02

--- practice/api/views.py
+++ practice/api/views.py
@@ -3,9 +3,35 @@ from django.shortcuts import render
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
+from rest_framework import status
+from rest_framework_simplejwt.tokens import RefreshToken
 
 
 @api_view(['GET'])
 @permission_classes([IsAuthenticated])
 def protected_view(request):
-    return Response({'message': 'Dostep przyznany (JWT OK)!'})
+    return Response({'message': 'Dostep przyznany (JWT OK)!'}, status=status.HTTP_200_OK)
+
+
+@api_view(['POST'])
+def logout(request):
+    try:
+        refresh_token = request.data["refresh"]
+        token = RefreshToken(refresh_token)
+        token.blacklist()
+        return Response({"message": "Successfully logged out"}, status=status.HTTP_205_RESET_CONTENT) 
+    # Dlaczego status 205 Reset Content a nie 200 OK?
+    # Bo 205 Reset Content jest bardziej semantyczny przy operacji wylogowania: oznacza, 
+    # że żądanie zakończyło się sukcesem, a klient powinien zresetować widok/formularz. 
+    # 200 OK też by działało, ale 205 dokładniej przekazuje intencję „zakończ sesję i oczyść stan po stronie klienta”.
+    except Exception as e:
+        return Response({"message": "Invalid token"}, status=status.HTTP_400_BAD_REQUEST)
+    
+
+# * Czy endpoint powinien wymagać **`IsAuthenticated`**?
+# Access Token wygasa szybciej Refresh Token co moze spowodować, ze uzytkownik nie bedzie mogl sie zalogowac bo bez access tokena IsAuthenticated nie przepuści i wywali błąd.
+# Jest to dodatkowa warstwa zabezpieczenia więc edpoint moze wymagać IsAuthenticated ale nie musi.
+   
+# * Co w logach chcesz zapisać (bezpieczeństwo vs. PII)?
+# Odp. Nie mozna logować wrazliwych danych takich jak tokeny, dane osobowe, hasła. Można logować informacje o błędach, adresy IP, 
+# timestampy, ale bez danych umożliwiających identyfikację użytkownika. Logi powinny być anonimowe i nie zawierać PII (Personally Identifiable Information) ze względów bezpieczeństwa i prywatności.
--- practice/practice/urls.py
+++ practice/practice/urls.py
@@ -20,16 +20,19 @@ from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
     TokenVerifyView,
+    TokenBlacklistView,
 )
-from api.views import protected_view
+from api.views import protected_view, logout
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+    path('api/token/blacklist/', TokenBlacklistView.as_view(), name='token_blacklist'),
 ]
 
 urlpatterns += [
     path('api/secure/', protected_view, name='secure'),
+    path('api/logout/', logout, name='logout'),
 ]
